16331d7557e39b7dd896f0dc91f8701b1943a764
Ma TianChi <chaseeer@gmail.com>
2025-11-25 15:07:14 +0900

    11

--- start-nginx.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# 设置默认端口为 8080（如果 PORT 环境变量未设置）
-export PORT=${PORT:-8080}
-# 替换 nginx 配置文件中的 PORT 占位符
-envsubst '${PORT}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
-# 启动 nginx
-exec nginx -g 'daemon off;'
-
